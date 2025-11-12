88b9ab00667ed77b665e7407341305bb3430ee2a
Varun <varun.varada2612@gmail.com>
2025-11-12 11:33:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
+
 # from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-from langchain.vectorstores import FAISS
+# from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import os, tempfile, streamlit as st
@@ -110,4 +112,5 @@ Resume Content: {resume_content}
 
     st.subheader("Match Score Result")
     st.write(result)
+
 
